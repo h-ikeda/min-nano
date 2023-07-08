a818bf216fc852bcfb5217113e7aecac264854c9
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2023-07-08 16:22:27 +0000

    インデントを3に統一

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import App from './App.vue';
 import { routes } from './routes';
 
 if (process.env.NODE_ENV === 'production' && location.hostname.toLowerCase().endsWith('.firebaseapp.com')) {
-  const { hostname } = location;
-  location.hostname = hostname.replace(/\.firebaseapp\.com$/i, '.web.app');
+   const { hostname } = location;
+   location.hostname = hostname.replace(/\.firebaseapp\.com$/i, '.web.app');
 }
 
 GlobalWorkerOptions.workerSrc = workerSrc;
 
 const app = createApp(App);
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+   history: createWebHistory(),
+   routes,
 });
 app.use(router);
 app.mount(document.body);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,vue}"],
-  theme: {
-    extend: {
-      keyframes: {
-        vibrate: {
-          '0%, 50%, 100%': { transform: 'translateX(0)' },
-          '25%': { transform: 'translateX(1rem)' },
-          '75%': { transform: 'translateX(-1rem)' },
-        },
+   content: ["./src/**/*.{html,js,vue}"],
+   theme: {
+      extend: {
+         keyframes: {
+            vibrate: {
+               '0%, 50%, 100%': { transform: 'translateX(0)' },
+               '25%': { transform: 'translateX(1rem)' },
+               '75%': { transform: 'translateX(-1rem)' },
+            },
+         },
+         animation: {
+            vibrate: 'vibrate 0.15s ease-in-out 2',
+         },
       },
-      animation: {
-        vibrate: 'vibrate 0.15s ease-in-out 2',
-      },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 }
